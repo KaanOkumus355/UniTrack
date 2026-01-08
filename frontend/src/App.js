4cdd5af2cb0ac3e78b3b41e237047104941adf57
Kaan Okumus <kaanokumus35@hotmail.com>
2026-01-08 23:19:32 +0300

    Did some frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function Header() {
-    return (
-        <h1>Hello World</h1>
-    );
+  return (
+    <div className="header">
+      <img src="UniTrackLogo.png" alt="UniTrack Logo" className="logo" /> 
+      <h1>UniTrack</h1>
+    </div>
+  );
 }
 
+function MiddleSection() {
+  return (
+    <div className="middle-section">
+      <h2>Welcome to UniTrack</h2>
+      <p>Your ultimate university management system.</p>
+      <div className="middle-section-buttons">
+        <button className="login-button">Login</button>
+        <button className="register-button">Register</button>
+      </div>
+    </div>
+  );
+}
+
+
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <MiddleSection />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+        <div className="loginpage">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Submit</button>
+        </div>
+    );
+}
+
+export default Login;
