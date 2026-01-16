84e202bb0264e4bf878e9451bd9211a5f3fd332d
Kaan Okumus <kaanokumus35@hotmail.com>
2026-01-16 20:09:14 +0300

    Started doing the fronend of main

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,8 +1,18 @@
 function Main({ username, onAddExam }) {
   return (
     <>
-      <h1>Welcome, {username}!</h1>
-      <span onClick={onAddExam}>Add Exam Time.</span>
+      <div className="side_content">
+        <ul>
+          <li className="active"><i class="fa fa-home" aria-hidden="true"></i>Dashboard</li>
+          <li><i class="fa fa-calendar" aria-hidden="true"></i>Weekly Planner</li>
+          <li><i class="fa fa-book" aria-hidden="true"></i>Courses</li>
+          <li><i class="fa fa-check-square" aria-hidden="true"></i>Tasks</li>
+        </ul>
+      </div>
+      <div className="main-container">
+        <h1>Welcome, {username}!</h1>
+        <span onClick={onAddExam}>Add Exam Time.</span>
+      </div>
     </>
   );
 }
